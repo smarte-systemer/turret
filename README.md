d5815c747f9d58ac1f819865a8a8c5966d1c27d5
Oess98 <oess-98@hotmail.com>
2023-08-21 17:36:55 +0200

    Initial commit
    - Added Sphinx docs

---
+++ README.md
@@ -0,0 +1,5 @@
+# Turret
+
+## Docstring
+
+[Docstring example](https://www.sphinx-doc.org/en/master/usage/extensions/example_google.html)
---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+# Test
+
+    :param client: A handle to the :class:`simpleble.SimpleBleClient` client
+    :type client: class:`simpleble.SimpleBleClient`
+
+"""
+
+print("Hello")
