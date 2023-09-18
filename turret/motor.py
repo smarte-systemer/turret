af166dfdab4b3f92a55cb1ea20ed038a9e33e9ae
Ole <238145@usn.no>
2023-09-18 16:17:12 +0100

    Added period calculation

--- turret/motor.py
+++ turret/motor.py
@@ -5,25 +5,21 @@ COUNTERCLOCKWISE = CCW = 1
 CLOCKWISE = CW = 0
 
 class motor:
-    def __init__(self, pull_pin: int, direction_pin: int, enable_pin: int) -> None:
+    def __init__(self, pull_pin: int, direction_pin: int, frequency: int) -> None:
         self.__pull_pin = pull_pin
         self.__direction_pin = direction_pin
-        self.__enable_pin = enable_pin
-
+        self.__period = 1/frequency
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__pull_pin, GPIO.OUT)
         GPIO.setup(self.__direction_pin, GPIO.OUT)
-        # GPIO.setup(self.__enable_pin, GPIO.OUT)
-
+      
     def drive(self, steps:int, direction: bool = True):
-        # GPIO.output(self.__enable_pin, GPIO.HIGH)
         GPIO.output(self.__direction_pin, direction)
         for step in range(steps):
             GPIO.output(self.__pull_pin, GPIO.HIGH)
-            sleep(5*10**(-3))# Sleep for 5ms
+            sleep(self.__period/2)# Sleep for 5ms
             GPIO.output(self.__pull_pin, GPIO.LOW)
-            sleep(5*10**(-3))# Sleep for 5ms
-
+            sleep(self.__period/2)# Sleep for 5ms
 
         
     
