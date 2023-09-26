f446dab87064abb5c32a337735e157bf9e47f7e3
Oess98 <oess-98@hotmail.com>
2023-09-26 09:12:30 +0200

    Added more functionality
    - Added drive_revolution to perform full revolutions
    	- This needs to get revolutions based on microstep setting
    	- Added __get_revolutions that returns the number of steps
    	  needed for a full revolutions.
    - Added enum to have a type for directions

--- turret/motor.py
+++ turret/motor.py
@@ -4,42 +4,38 @@ from enum import Enum
 
 
 class Direction(Enum):
-    """Enum for directions
+        """Enum for directions
+        """
+        COUNTERCLOCKWISE = CCW = 1
+        CLOCKWISE = CW = 0
+
+
+class Motor:
+    """Constructor for motor driver
 
     Args:
-        Enum: _description_
+        pulse_pin: Pin used to pulse
+        direction_pin: Pin used to assign direction
+        frequency: Frequency to pulse, higher -> faster.
+        microstep: Which mirostep setting. Defaults '200' 
     """
-    COUNTERCLOCKWISE = CCW = 1
-    CLOCKWISE = CW = 0
-
-class motor:
-    """ 
-    Interface for TB6600 motor drive
-        Constructor function will set pins to output with [BCM](https://www.raspberrypi.com/documentation/computers/raspberry-pi.html) configuration.
-    """
-
-    def __init__(self, pulse_pin: int, direction_pin: int, frequency: int) -> None:
-        """Constructor
-
-        Args:
-            pulse_pin: Pin used to pulse
-            direction_pin: Pin used to determine direction(True/False).
-            frequency: _description_
-        """
+    def __init__(self, pulse_pin: int, direction_pin: int, frequency: int, microstep: str = '200') -> None:
         self.__pulse_pin = pulse_pin
         self.__direction_pin = direction_pin
         self.__period = 1/frequency
+        self.__steps_per_revolutions = self.__get_revolutions(microstep)
+        
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.__pulse_pin, GPIO.OUT)
         GPIO.setup(self.__direction_pin, GPIO.OUT)
-      
-
-    def drive(self, steps:int):# direction: Direction = Direction.CLOCKWISE):
+        
+        
+    def drive(self, steps: int, direction: Direction = Direction.CLOCKWISE):
         """Drive motor in one direction for a number of steps
 
         Args:
             steps: Number of steps to rotate
-            direction:  Direction to rotate. Defaults to clockwise.
+            direction:  Direction to rotate. Defaults to Direction.CLOCKWISE.
         """
         GPIO.output(self.__direction_pin, direction)
         for step in range(steps):
@@ -47,3 +43,37 @@ class motor:
             sleep(self.__period/2)
             GPIO.output(self.__pulse_pin, GPIO.LOW)
             sleep(self.__period/2)
+
+    def drive_revolution(self, revolutions: int, direction: Direction = Direction.CLOCKWISE):
+        """Perform full revolutions
+
+        Args:
+            revolutions: Number of revolutions to perform
+            direction: Direction to rotate in. Defaults to Direction.CLOCKWISE.
+        """
+        for revolution in range(revolutions):
+             self.drive(self.__steps_per_revolutions, direction)
+    def __get_revolutions (self, microstep: str):
+        """Matches microstep setting to steps per revolution
+
+        Args:
+            microstep: DIP switch setting
+
+        Returns:
+            Number of steps per revolution
+        """
+        match microstep:
+              case '1':
+                   return 200
+              case '2/A':
+                   return 400
+              case '2/B':
+                   return 400
+              case '4':
+                   return 800
+              case '8':
+                   return 1600
+              case '16':
+                   return 3200
+              case '32':
+                   return 6400
