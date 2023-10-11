26504839bdee0f94e666d56c4bba9aa11e1186b2
chrisdaff93 <99039245+chrisdaff93@users.noreply.github.com>
2023-10-11 14:24:00 +0200

    Added file sharedqueueprocesses.py for multithreading
    Just a commit and push for continue work from home computer

---
+++ sharedqueueprocesses.py
@@ -0,0 +1,15 @@
+from multiprocessing import shared_memory
+from turret.motor import Motor
+import cv2
+import tflite_support.task 
+import core, processor, vision
+
+
+def write_coordinates():
+    return
+
+def write_boudingbox():
+    return
+
+def move_turret():
+    return
